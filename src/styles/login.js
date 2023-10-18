f880fe050db26110afca677c6d7b2db8764f71a3
Ni-aina <niandriah.l@gmail.com>
2023-10-17 22:59:39 -0400

    Login

--- src/styles/login.js
+++ src/styles/login.js
@@ -2,21 +2,21 @@ export const formInput = {
     marginBottom: 3,
     width: 300,
     border: "1px solid #FFF",
-    borderRadius: 1.5
+    borderRadius: 1.5,
+    "&:focus-within": {
+        border: "none"
+    }
 }
 export const inputLabelprops = {
     sx: {
         color: "#FFF",
         "&.Mui-focused": {
-            color: "#FFF"
+            // color: "#FFF"
         }
     }
 }
 export const inputProps = {
     sx: {
-        color: "#FFF",
-        "&:focus": {
-            border: "none"
-        }
+        color: "#FFF"
     }
 }
